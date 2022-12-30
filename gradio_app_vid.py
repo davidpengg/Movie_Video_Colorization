9cccde95378a19f496d94d16608a8448ee6cd08a
David Peng <david.peng@yale.edu>
2022-12-30 00:58:01 -0500

    working nicely! TODO: fps customization

--- gradio_app_vid.py
+++ gradio_app_vid.py
@@ -7,25 +7,31 @@ from colorizer_net import *
 from pdb import set_trace
 
 with torch.no_grad():
-    model = MainModel()
-    # model = torch.load("modelbatchv2.pth", map_location=device)
-    model.load_state_dict(
-        torch.load(
-            "modelbatchv2.pth", 
-            map_location=torch.device('cpu')
-        ).state_dict()
-    )
-    assert model.device.type == "cpu"
+    # model = MainModel()
+    # model.load_state_dict(
+    #     torch.load(
+    #         "modelbatchv2.pth", 
+    #         map_location=torch.device('cpu')
+    #     ).state_dict()
+    # )
+    # assert model.device.type == "cpu"
+
+    model = torch.load("modelbatch_epoch9.pth", map_location=torch.device('cpu'))
+
     model.eval()
 
-def inference(vid):
-
+def inference(vid, fps):
     return inference_model_vid(model, vid)
 
+# gr.Slider(minimum=1, maximum=30, value=24, step=1, label="FPS of Colorized Video"),
+# gr.Radio([2, 5, 12, 24, 30], value=5, label="FPS of Colorized Video"),
 demo = gr.Interface(
     inference, 
-    inputs=gr.Video(), 
-    outputs=gr.Video(),
+    inputs=[
+        gr.Video(label="Black and White"),
+        gr.Dropdown([2, 6, 12, 24, 30], value=6, label="FPS of Colorized Video"),
+    ], 
+    outputs=gr.Video(label="Colorized"),
     title="DCGANs Video Colorizer",
     description="Demo of Video Colorization Model",
     allow_flagging='never',
--- utils.py
+++ utils.py
@@ -10,14 +10,16 @@ from pytube import YouTube
 
 import os
 from tqdm import tqdm
-from moviepy.editor import AudioFileClip, VideoFileClip
+from moviepy.editor import VideoFileClip
 import cv2
+from pathlib import Path
 
 from pdb import set_trace
 
 def get_progvid_from_youtube(youtube_url, path_progvid):
     """Creates progvid file from YouTube url.
     """
+    # wiz of oz: https://www.youtube.com/watch?v=PSZxmZmBfnU
     yt = YouTube(youtube_url)
     x = yt.streams.filter(progressive=True, file_extension='mp4')
     print(x)
@@ -27,7 +29,6 @@ def lab_to_rgb(L, ab):
     """
     Takes a batch of images
     """
-    # set_trace()
     L = (L + 1.) * 50.
     ab = ab * 110.
     Lab = torch.cat([L, ab], dim=1).permute(0, 2, 3, 1).cpu().numpy()
@@ -38,73 +39,85 @@ def lab_to_rgb(L, ab):
     return np.stack(rgb_imgs, axis=0)
 
 SIZE = 256
-def inference_model_img(model, img):
-    # set_trace()
-    # save old size
 
-    if not isinstance(img, Image.Image):
-        # set_trace()
-        img = Image.fromarray(img)
-    og_size = img.size
-    # resize img to be SIZE x SIZE
+def get_L(img):
     img = transforms.Resize((SIZE, SIZE),  transforms.InterpolationMode.BICUBIC)(img)
-
     img = np.array(img)
     img_lab = rgb2lab(img).astype("float32")
     img_lab = transforms.ToTensor()(img_lab)
     L = img_lab[[0], ...] / 50. - 1. # Between -1 and 1
-    # ab = img_lab[[1, 2], ...] / 110. # Between -1 and 1
 
-    L = L[None]
-    # ab = ab[None]
-    # data = {"L": L, "ab": ab}
+    return L
 
-    #   Load image into model
-    # model.setup_input(data)
-    model.L = L.to(model.device)
+def get_predictions(model, L):
+    # model.L = L.to(model.device)
+    model.L = L.to(torch.device('cpu'))
     model.forward()
-
     fake_color = model.fake_color.detach()
-    # real_color = model.ab
-    # L = model.L
     fake_imgs = lab_to_rgb(L, fake_color)
-    # real_imgs = lab_to_rgb(L, real_color)
 
-    fake_img = fake_imgs[0]
+    return fake_imgs
 
-    # get original size
-    fake_img = skimage.transform.resize(fake_img, og_size[::-1])
+def inference_model_img(model, img):
+    L = get_L(img)
+    L = L[None] # put in list
+    fake_imgs = get_predictions(model, L)
+    fake_img = fake_imgs[0] # get out of list
+    resized_fake_img = skimage.transform.resize(fake_img, img.size[::-1])
 
-    return fake_img
+    return resized_fake_img
 
-def inference_model_vid(model, vid: str):
-    # iter_frames is an iterator
+# def inference_model_batch_imgs(model, imgs):
+#     L = get_L(imgs)
+#     fake_imgs = get_predictions(model, L)
+#     resized_fake_imgs = [skimage.transform.resize(fake_img, imgs[0].size[::-1]) for fake_img in fake_imgs]
+    
+#     return resized_fake_imgs
 
-    input_video = VideoFileClip(vid)
+def inference_model_vid(model, path_input: str, fps=None):
+    print(path_input)
+
+    input_video = VideoFileClip(path_input)
 
     colorized_frames = []
     nframes = input_video.reader.nframes
     print(f"There are {nframes} frames to process")
-    for i, frame in tqdm(enumerate(input_video.iter_frames()), total=nframes):
-    
-        color_frame = inference_model_img(model, frame)
+
+    frames = input_video.iter_frames()
+    for frame in tqdm(frames, total=nframes):
+        color_frame = inference_model_img(model, Image.fromarray(frame))
         colorized_frames.append(color_frame)
-    
+
     height, width = colorized_frames[0].shape[:2]
     size = (width, height)
-    set_trace()
-    out = cv2.VideoWriter('tmp_output.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 24, size)
 
-    for colorized_frame in colorized_frames:
-        scaled_colorized_frame = (colorized_frame*255).astype(np.uint8)
-        out.write(scaled_colorized_frame)
+    # copy audio from input
+    input_audio = input_video.audio
+
+    # create tmp path that is same as input path but with '_out.mp4'
+    path_input_obj = Path(path_input)
+    path_tmp = str(path_input_obj.with_stem(path_input_obj.stem + "_tmp"))
+
+    out = cv2.VideoWriter(path_tmp, cv2.VideoWriter_fourcc(*'mp4v'), input_video.fps, size)
+
+    if colorized_frames[0].max() <= 1:
+        print("scaling up to 255 range...")
+        colorized_frames = [(colorized_frame*255).astype(np.uint8) for colorized_frame in colorized_frames]
+
+    for colorized_frame in tqdm(colorized_frames):
+        # use RGB, not RBG!
+        rgb_colorized_frame = cv2.cvtColor(colorized_frame, cv2.COLOR_BGR2RGB)
+        out.write(rgb_colorized_frame)
     out.release()
-    set_trace()
 
-    # add audio too
-    output_video = VideoFileClip("tmp_output.mp4")
-    output_video = output_video.set_audio(input_video.audio)
-    output_video.write_videofile("output.mp4")
+    # create output path that is same as input path but with '_out.mp4'
+    path_output = str(path_input_obj.with_stem(path_input_obj.stem + "_out"))
 
-    os.remove("tmp_output.mp4")
-    return "output.mp4"
+    output_video = VideoFileClip(path_tmp)
+    output_video = output_video.set_audio(input_audio)
+    output_video.write_videofile(path_output, logger=None)
+
+    os.remove(path_tmp)
+
+    print("Done.")
+    return path_output
